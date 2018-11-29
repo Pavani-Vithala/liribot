abdfbe02dbf534870b81b2455c0200abb3004573
Pavani Vithala <pavani.vithala18@gmail.com>
2018-11-28 22:58:45 -0500

    Added Spotify API

--- liri.js
+++ liri.js
@@ -4,10 +4,11 @@ var keys = require("./keys");
 //console.log(keys);
 var axios = require("axios");
 var fs = require("fs");
-
+var Spotify = require("node-spotify-api");
 
 var command = process.argv[2];
 var param = process.argv.slice(3).join(" ");
+var text = command + " " +param + "\n";
 //console.log(param);
 
 
@@ -15,15 +16,23 @@ switch (command) {
     case "concert-this":
     var artist = param;
         concertThis(artist);
+        
         break;
-   /* case "spotify-this-song":
-        console.log("Entered spotify-this-song function");
-        var spotify = keys.spotify.id;
-        console.log("The spotify id is" + spotify);
-        spotifyURL = "https://api.spotify.com/search/results/" + param + "&type=track&key=" + spotify;
-        // http://ws.spotify.com/search/1/track?q=Tori+Amos+Precious+Things
-        console.log(spotifyURL);
-        break;*/
+    case "spotify-this-song":
+    
+    var song = param;
+    var spotify = new Spotify(keys.spotify);
+    //console.log(spotify);
+     
+    spotify.search({ type: 'track', query: song,limit:1 }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      var spotifyResult = Object.values(data.tracks)//["artists"][0])//.tracks["album"][0]);
+     logText(spotifyResult);
+    console.log(Object.values(spotifyResult)); 
+    });
+        break;
     case "movie-this":
 
         if (param == "") {
@@ -62,6 +71,7 @@ function getMovieDetails(moviename) {
         function (response) {
             // console.log(response);
             var result = response.data;
+            logText(result);
             console.log("Title : " + result.Title);
             console.log("Year the movie came out: " + result.Year);
             console.log("IMDB Rating: " + result.imdbRating);
@@ -92,9 +102,30 @@ function concertThis(){
                 var country = venueDetails["country"];
                 var location = venueDetails["city"];
                 console.log("Venue Details:" + "  " + nameOfVenue + " " + location + " " + country + " " + "on" + " " + concertDate);
+                logText(result);
             }
 
         });
 
 
+}
+function logText(result)
+{
+    var logText = "\n" + text + " " + "\n";
+    var objectText =  JSON.stringify(result);
+    var finalText =  logText + "\n" + objectText;
+    fs.appendFile("log.txt", finalText, function(err) {
+
+        // If an error was experienced we will log it.
+        if (err) {
+          console.log(err);
+        }
+      
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+            
+          console.log("Content Added!");
+        }
+      
+      }); 
 }

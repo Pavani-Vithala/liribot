6a38589a4d5543497c2562e7c00e5ac38b2b6552
Pavani Vithala <pavani.vithala18@gmail.com>
2019-04-07 18:57:29 -0400

    Cleaned code

--- liri.js
+++ liri.js
@@ -1,31 +1,32 @@
 require("dotenv").config();
 var moment = require("moment");
 var keys = require("./keys.js");
-//console.log(keys);
 var axios = require("axios");
 var fs = require("fs");
 var Spotify = require("node-spotify-api");
-
+//Variable which captures user's choice
 var command = process.argv[2];
+//If parameter 3 exits, this command is to ignore the spaces in the words like a song and consider it as a single  parameter
 var param = process.argv.slice(3).join(" ");
 var text = command + " " + param + "\n";
-//console.log(param);
+
 
 
 switch (command) {
     case "concert-this":
         var artist = param;
+        //If user's choice is to concert a song, call the function concertThis()
         concertThis(artist);
 
         break;
     case "spotify-this-song":
-
+        //If user's choice is to get a song details from Spotify API, call the function spotifyThisSong()
         spotifyThisSong(param);
 
-        
+
         break;
     case "movie-this":
-
+        //If user do not provide a command, this is the default, display the details of Mr.Nobody movie by calling getMovieDetails function
         if (param == "") {
             param = "Mr.Nobody";
             getMovieDetails(param);
@@ -36,29 +37,36 @@ switch (command) {
 
         break;
     case "do-what-it-says":
+        //if the choice is do-what-it-says, read the command from a text file and execute
         console.log("Entered do-what-it-says function");
         fs.readFile("random.txt", "utf8", function (error, data) {
             if (error) {
                 return console.log(error);
             }
+            //get the string from text file and split by comma
             var dataArr = data.split(",");
-            var dataArr1 = dataArr[1].slice();//.join(" ");
+            //store the split elements in an array called dataArr1
+            var dataArr1 = dataArr[1].slice();
             console.log(dataArr1);
+            //If the first value of dataArr is movie-this, call the function getMovieDetails and pass the dataArr1 as arguement
             if (dataArr[0] == "movie-this")
                 getMovieDetails(dataArr1);
+            //If the first value of dataArr is concert-this, call the function concertThis and pass the dataArr1 as arguement
             if (dataArr[0] == "concert-this")
                 concertThis(dataArr1);
-            if(dataArr[0] == "spotify-this-song")
-            spotifyThisSong(dataArr1);
+            //If the first value of dataArr is spotify-this-song, call the function spotifyThisSong and pass the dataArr1 as arguement
+            if (dataArr[0] == "spotify-this-song")
+                spotifyThisSong(dataArr1);
         });
         break;
 
 }
 
+//Function to display the movie details from the omDB api by passing the movie name as parameter
 function getMovieDetails(moviename) {
 
     var queryURL = "http://www.omdbapi.com/?t=" + moviename + "&y=&plot=short&apikey=" + process.env.OMDB_APIKEY;
-
+    //Axios call to omdb API to get details like title,year,imdbrating,Country,Language,Plot and Actors
     axios.get(queryURL).then(
         function (response) {
             // console.log(response);
@@ -75,10 +83,13 @@ function getMovieDetails(moviename) {
         });
 
 }
-function concertThis() {
+
+//Function to display event details from bandsintown APi by passing artist name
+function concertThis(artist) {
 
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + process.env.BANDSINTOWN_APIKEY;
-    //console.log(queryURL);
+
+    //Axios call to get details of the event like venue ,country,city
     axios.get(queryURL).then(
         function (response) {
 
@@ -86,8 +97,6 @@ function concertThis() {
 
             for (var i = 0; i < result.length; i++) {
                 var dateTime = result[i]["datetime"];
-                // console.log("datetime from object is " + dateTime);
-                //console.log("Name of the artist:" + artist);
                 var concertDate = moment(dateTime).format('L');
                 var venueDetails = result[i]["venue"];
                 var nameOfVenue = venueDetails["name"];
@@ -101,10 +110,13 @@ function concertThis() {
 
 
 }
+
+//Function to log all the results of the commands in a text file
 function logText(result) {
     var logText = "\n" + text + " " + "\n";
     var objectText = JSON.stringify(result);
     var finalText = logText + "\n" + objectText;
+    //appendFile to append the contents and avoid overwriting
     fs.appendFile("log.txt", finalText, function (err) {
 
         // If an error was experienced we will log it.
@@ -120,19 +132,21 @@ function logText(result) {
 
     });
 }
-function spotifyThisSong(param)
-{
+
+//Function to display the details of a song from spotify API base don the song name
+function spotifyThisSong(param) {
     var song = param;
+    //create a Spotify API key from Spotify object in keys file
     var spotify = new Spotify(keys.spotify);
-    //console.log(spotify);
-    if(param == "")
-    {
+    //If no song name is passed, display details for The Sign song
+    if (param == "") {
         song = "The Sign";
     }
+    //Pass the song name and search the spotify DB
     spotify.search({ type: 'track', query: song })
         .then(function (response) {
             var result = response.tracks.items[0];
-            //console.log(result);
+
             var artist = result.artists[0].name;
             console.log("Name of the Artist(s):" + artist);
             console.log("Name of the song:" + song);
@@ -140,6 +154,7 @@ function spotifyThisSong(param)
             console.log("Link to the song: " + songLink);
             var album = result.album["name"];
             console.log("Album the song is from :" + album);
+            //Log the results in the text file for logging
             logText(result);
         })
         .catch(function (err) {
